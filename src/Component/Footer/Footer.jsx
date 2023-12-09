607af1e0fb49e78d25bb3f53a9d4bdf1d53bfc8c
abeeratia <abeeryousef198@gmail.com>
2023-12-09 22:06:39 +0200

    footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='d-flex justify-content-center align-items-center bg-success  '>
+    <div className='d-flex fixed-bottom justify-content-center align-items-center bg-success  '>
     
-    <h2 className='text-light'>Footer</h2>
+    <h2 className='text-light '>Footer</h2>
     
     </div>
   )
